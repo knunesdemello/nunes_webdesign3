758519141423b9d4bdca794a6b9c655a6c4c956c
knunesdemello <nunes_karina_s@student.smc.edu>
2020-04-07 12:20:30 -0700

    scrollspy update
    not quite there yet, but added scrollspy. Still needs JS

--- homework/week_6_midterm/script.js
+++ homework/week_6_midterm/script.js
@@ -1,42 +1,8 @@
 $(document).ready(function(){
 
-    $('#myCarousel').carousel({
-        interval: 330
-    });
-
-    $('#carouselControl').click(function(){
-
-        if( $('#carouselControl').hasClass('paused') ){
-            $('#myCarousel').carousel('cycle');
-            $('#carouselControl').text('Pause');
-        }
-        else {
-            $('#myCarousel').carousel('pause');
-            $('#carouselControl').text('Play');
-        }
-
-        $('#carouselControl').toggleClass('paused');
-    });
 
 
-    $('#modalLauncher').click(function(){
-        $('#exampleModal').modal({
-            backdrop: false
-        });
-    });
-
-    
-        var selectedClass = "gallery";
-        $(".filter").click(function(){
-        selectedClass = $(this).attr("data-rel");
-        $("#gallery").fadeTo(100, 0.1);
-        $("#gallery div").not("."+selectedClass).fadeOut().removeClass('animation');
-        setTimeout(function() {
-        $("."+selectedClass).fadeIn().addClass('animation');
-        $("#gallery").fadeTo(300, 1);
-        }, 300);
-        });
-
+    $('body').scrollspy({target: ".navbar"})
 
 }); 
   
