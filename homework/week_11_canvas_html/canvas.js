f76c05ab3a383e2b45340b1cfc6a924a16b91a3d
knunesdemello <nunes_karina_s@student.smc.edu>
2020-05-19 08:23:24 -0700

    Homework Week_11 HTML_CANVAS
    ball color change on button click; change ball speed on ball click; score count on ball click;

---
+++ homework/week_11_canvas_html/canvas.js
@@ -0,0 +1,94 @@
+var canvas = document.getElementById("myCanvas");
+    var context = canvas.getContext("2d");
+
+    var ballColor = 'rgb(255, 255, 0)';
+    var ballPosX = canvas.width/2;
+    var ballPosY = canvas.height/2;
+    var ballRadius = 50;
+
+    var ballSpeedX = 4;
+    var ballSpeedY = 2;
+
+
+    context.beginPath();
+    context.arc(ballPosX, ballPosY, ballRadius, 0, 2*Math.PI);
+    context.fill();
+
+    var animate = function(){
+      context.clearRect(0, 0, canvas.width, canvas.height);
+
+
+      context.fillStyle = ballColor;
+      context.beginPath();
+      context.arc(ballPosX, ballPosY, ballRadius, 0, 2*Math.PI);
+      context.fill();
+
+      if((ballPosX + ballRadius >= canvas.width) || (ballPosX - ballRadius) <= 0){
+        ballSpeedX = ballSpeedX * -1;
+      }
+
+      if( ( ballPosY + ballRadius >= canvas.height) || ( ballPosY - ballRadius <= 0) ){
+        ballSpeedY = ballSpeedY * -1;
+      }
+
+      ballPosX = ballPosX + ballSpeedX;
+      ballPosY = ballPosY + ballSpeedY;
+
+
+      window.requestAnimationFrame(animate);
+
+    }
+
+
+    animate();
+
+//  CHANGE BALL SPEED ON SIZE BUTTON CLICK
+canvas.addEventListener("click", function (event) {
+    var distX = Math.abs(ballPosX - event.offsetX);
+    var distY = Math.abs(ballPosY - event.offsetY);
+ 
+    if (distX < ballRadius && distY < ballRadius) {
+      ballSpeedX = (Math.floor(Math.random() * 8));
+    }
+ 
+    if (distX < ballRadius && distY < ballRadius) {
+      ballColor = colors[Math.floor(Math.random() * colors.length)];
+    }
+  })
+
+
+
+  colorButton.addEventListener("click", function (event) {
+    var colors = ['#D9FFEC', '#FF99A5', '#CC99B8'];
+    ballColor = colors[Math.floor(Math.random() * colors.length)];
+  });
+
+  document.getElementById("colorButton").onclick = function(){
+	document.getElementById("output").style.colors;
+}
+
+sizeButton.addEventListener("click", function changesize(event) {
+        var el = document.getElementById("output");
+        el.style.height = "200px";
+        el.style.width = "200px";
+
+  });
+ 
+  //  SCORE COUNT
+  var button = document.getElementById("score"),
+    count = 0;
+  canvas.onclick = function () {
+    var distX = Math.abs(ballPosX - event.offsetX);
+    var distY = Math.abs(ballPosY - event.offsetY);
+    if (distX < ballRadius && distY < ballRadius) {
+      count += 1;
+      button.innerHTML =  + count;
+    }
+  };
+
+
+
+
+  
+  
+  '#D9FFEC', '#FF99A5', '#CC99B8';
